b7f7a5b163af0b44cb092d2dba2aba85deb16183
adsfgg <11364579+adsfgg@users.noreply.github.com>
2021-06-05 22:04:51 +0100

    Nanoshield changs

--- src/lua/CompMod/Mixins/NanoShieldMixin.lua
+++ src/lua/CompMod/Mixins/NanoShieldMixin.lua
@@ -14,3 +14,30 @@ function NanoShieldMixin:GetNanoShieldTimeRemaining()
 
     return percentLeft
 end
+
+local oldOnProcessMove = NanoShieldMixin.OnProcessMove
+function NanoShieldMixin:OnProcessMove(input)
+    oldOnProcessMove(self, input)
+    self:UpdateNanoshieldWelding(input.time)
+end
+
+local oldOnUpdate = NanoShieldMixin.OnUpdate
+function NanoShieldMixin:OnUpdate(deltaTime)
+    oldOnUpdate(self, deltaTime)
+    self:UpdateNanoshieldWelding(deltaTime)
+end
+
+function NanoShieldMixin:UpdateNanoshieldWelding(deltaTime)
+    if Server then
+        if self:GetIsNanoShielded() then
+            if self:isa("Player") then
+                if self:GetArmor() < self:GetMaxArmor() then
+                    local weldRate = kPlayerArmorWeldRate * (self:GetIsInCombat() and 0.1 or 0.5)
+                    self:SetArmor(self:GetArmor() + (weldRate * deltaTime))
+                end
+            else
+                self:AddHealth(MAC.kRepairHealthPerSecond * deltaTime)
+            end
+        end
+    end
+end
